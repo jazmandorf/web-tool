e6452ed8e1705f154b507c491957cba91b8f664f
jazmandorf <thaeao@Jazui-MacBookPro.local>
2019-09-23 14:45:47 +0900

    template files add

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c ec
 
 func main() {
 	e := echo.New()
+	e.Static("/assets", "./src/static/assets")
 	renderer := &TemplateRender{
 		templates: template.Must(template.ParseGlob(`./src/views/*.html`)),
 	}
@@ -35,11 +36,23 @@ func main() {
 		return c.Render(http.StatusOK, "template.html", map[string]interface{}{
 			"name":    "Dolly!",
 			"reverse": 1234,
+			"foobar":  "",
 		})
 	})
 
 	e.GET("/hello", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "hello.html", map[string]interface{}{
+			"Name": myStruct{Name: "Dennis", Age: 36, Height: 170},
+		})
+	})
+
+	e.GET("/dashboard", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "dashboard_2.html", map[string]interface{}{
+			"Name": myStruct{Name: "Dennis", Age: 36, Height: 170},
+		})
+	})
+	e.GET("/dashboard2", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "dashboard_3.html", map[string]interface{}{
 			"Name": myStruct{Name: "Dennis", Age: 36, Height: 170},
 		})
 	})

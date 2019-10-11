ae3a37c84ed779ad42d3dab9f663c05a31de0dc4
Jaz <thaeao@JazuiMaBookPro2.kornet>
2019-10-11 18:55:25 +0900

    modified main

--- main.go
+++ main.go
@@ -82,7 +82,14 @@ func main() {
 	})
 
 	e.GET("/getHtml", func(c echo.Context) error {
-		res, err := http.NewRequest("GET", "http://localhost:1234/initial", nil)
+		url := "http//localhost:1234/initial"
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		client := &http.Client{}
+		res, err := client.Do(req)
 		if err != nil {
 			log.Fatal(err)
 		}

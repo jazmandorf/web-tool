10afc9f8a35ed52dd40fb721c9414b384d9733f3
Jaz <thaeao@JazuiMaBookPro2.kornet>
2019-10-11 18:52:27 +0900

    modified server

--- server.go
+++ server.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/labstack/echo"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.status, "Hello World")
-	})
-	e.Logger.Fatal(e.Start(":1234"))
+	url := "http://localhost:1234/getTest"
+
+	proxyReq, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{}
+	proxyRes, err := client.Do(proxyReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer proxyRes.Body.Close()
+
+	bytes, _ := ioutil.ReadAll(proxyRes.Body)
+	str := string(bytes)
+	fmt.Println(str)
 
 }
